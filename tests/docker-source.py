acae6945c3ce53690c464bfb12bb6f984b3ef548
James Ennis <james.ennis@codethink.com>
2018-07-03 12:24:16 +0100

    docker_source.py: Ensure docker-build test is an integration test
      - Also a change to the script element which copies the
      os-release file into the install root

--- tests/docker-source.py
+++ tests/docker-source.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 
-from tests.testutils import cli_integration as cli
+from tests.testutils import cli_integration, cli
 from tests.testutils.integration import assert_contains
 
 
@@ -10,16 +10,18 @@ DATA_DIR = os.path.join(
     "project"
 )
 
+@pytest.mark.integration
 @pytest.mark.datafiles(DATA_DIR)
-def test_docker_source_build(cli, datafiles):
+def test_docker_source_build(cli_integration, datafiles):
+
     project = os.path.join(datafiles.dirname, datafiles.basename)
-    checkout = os.path.join(cli.directory, 'checkout')
+    checkout = os.path.join(cli_integration.directory, 'checkout')
     element_name = 'docker-source/docker-source-test.bst'
 
-    result = cli.run(project=project, args=['build', element_name])
+    result = cli_integration.run(project=project, args=['build', element_name])
     assert result.exit_code == 0
 
-    result = cli.run(project=project, args=['checkout', element_name, checkout])
+    result = cli_integration.run(project=project, args=['checkout', element_name, checkout])
     assert result.exit_code == 0
 
     assert_contains(checkout, ['/etc/os-release'])
