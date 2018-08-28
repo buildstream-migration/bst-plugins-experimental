f5bb2ac9d511a93c8709c32b70d24d3a57a0c15c
Jonathan Maw <jonathan.maw@codethink.co.uk>
2018-08-28 18:03:46 +0100

    Update version to 0.4

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except ImportError:
     sys.exit(1)
 
 setup(name='BuildStream-external',
-      version="0.3.1",
+      version="0.4",
       description="A collection of BuildStream plugins that don't fit in with the core plugins for whatever reason.",
       license='LGPL',
       packages=find_packages(exclude=['tests', 'tests.*']),
