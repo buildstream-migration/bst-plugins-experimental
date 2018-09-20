040700d4166dda5847487fe91dfb5b961974f84d
Valentin David <valentin.david@codethink.co.uk>
2018-09-21 00:11:22 +0200

    Add entry point to collect-integration plugin.
    freedesktop-sdk fails with error:
    Pip package buildstream-external does not contain a plugin named
    'collect-integration'

--- setup.py
+++ setup.py
@@ -50,7 +50,8 @@ setup(name='BuildStream-external',
               'dpkg_build = bst_external.elements.dpkg_build',
               'dpkg_deploy = bst_external.elements.dpkg_deploy',
               'flatpak_image = bst_external.elements.flatpak_image',
-              'x86image = bst_external.elements.x86image'
+              'x86image = bst_external.elements.x86image',
+              'collect_integration = bst_external.elements.collect_integration'
           ]
       },
       setup_requires=['pytest-runner', 'setuptools_scm'],
