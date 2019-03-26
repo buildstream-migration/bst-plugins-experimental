040c74f38f03a2d6478705b4061a02456a024383
James Ennis <james.ennis@codethink.co.uk>
2019-03-26 15:34:08 +0000

    tests/quilt: Tidy up quilt tests

--- tests/quilt.py
+++ tests/quilt.py
@@ -2,10 +2,7 @@ import os
 import pytest
 
 from tests.testutils import cli_integration as cli
-from tests.testutils import plugin_import
 from tests.testutils.integration import assert_contains
-
-#pytestmark = pytest.mark.integration
 
 DATA_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -13,15 +10,14 @@ DATA_DIR = os.path.join(
 )
 
 @pytest.mark.datafiles(DATA_DIR)
-def test_quilt_build(cli, datafiles, plugin_import):
+def test_quilt_build(cli, datafiles):
     project = os.path.join(datafiles.dirname, datafiles.basename)
-    checkout = os.path.join(cli.directory, 'checkout')
-    element_name = 'quilt-build/quilt-build-test.bst'
+    checkout = os.path.join(cli.directory, 'quilt_checkout')
 
-    result = cli.run(project=project, args=['build', element_name])
+    result = cli.run(project=project, args=['build', "quilt-build-test.bst"])
     assert result.exit_code == 0
 
-    result = cli.run(project=project, args=['checkout', element_name, checkout])
+    result = cli.run(project=project, args=['checkout', "quilt-build-test.bst", checkout])
     assert result.exit_code == 0
 
     assert_contains(checkout, ['/patches/series','/patches/test','/src/hello.c'])
