663b10f705283447f4a24c3261ebc5d2aba7b3ad
Thomas Coldrick <othko97@gmail.com>
2019-08-05 11:47:34 +0100

    cmake tests: Use local testutils

--- tests/cmake.py
+++ tests/cmake.py
@@ -4,7 +4,7 @@
 import os
 import pytest
 
-from buildstream.testing import cli_integration as cli  # pylint: disable=unused-import
+from tests.testutils import cli_integration as cli  # pylint: disable=unused-import
 from buildstream.testing.integration import assert_contains
 from buildstream.testing._utils.site import HAVE_SANDBOX
 
