094408c82146e85f8e41b2c5117ad82b502020de
Benjamin Schubert <bschubert15@bloomberg.net>
2019-08-09 13:43:01 +0100

    conftests: Remove custom integration cache and use BuildStream's

--- tests/cmake.py
+++ tests/cmake.py
@@ -5,7 +5,7 @@ import os
 import pytest
 
 from buildstream.testing.runcli import cli_integration as cli  # pylint: disable=unused-import
-from buildstream.testing.integration import assert_contains
+from buildstream.testing.integration import assert_contains, integration_cache
 from buildstream.testing._utils.site import HAVE_SANDBOX
 
 
