1faaf636d9b6c44eae218f937dc8d75e1015db98
Benjamin Schubert <bschubert15@bloomberg.net>
2019-08-09 11:34:00 +0100

    Update BuildStream version

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,14 @@
 from buildstream.testing import sourcetests_collection_hook, register_repo_kind
 from tests.ostreerepo import OSTree
 
+
+# TODO: can we get this from somewhere? pkg_resources?
+package_name = "bst_plugins_experimental"
+
+
 # Register a repo type to run the ostree plugin through buildstream's
 # generic source plugin tests
-register_repo_kind('ostree', OSTree)
+register_repo_kind('ostree', OSTree, package_name)
 
 
 # This hook enables pytest to collect the templated source tests from
