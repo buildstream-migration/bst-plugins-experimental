c8a35c0a97b330e922ed74e9cc6b92a76a84a3cc
Thomas Coldrick <thomas.coldrick@codethink.co.uk>
2019-09-23 12:49:06 +0100

    tests: Add tests for tar_element checksums

--- tests/integration/tar_element.py
+++ tests/integration/tar_element.py
@@ -1,8 +1,10 @@
 # Pylint doesn't play well with fixtures and dependency injection from pytest
 # pylint: disable=redefined-outer-name
 
+import hashlib
 import os
 import tarfile
+
 import pytest
 
 from buildstream.testing.integration import assert_contains
@@ -17,16 +19,20 @@ DATA_DIR = os.path.join(
 )
 
 
+BLOCKSIZE = 65536
+
+
 @pytest.mark.datafiles(DATA_DIR)
 def test_tar_build(cli, datafiles):
     project = str(datafiles)
     checkout_dir = os.path.join(cli.directory, 'tar_checkout')
     tarpath = os.path.join(checkout_dir, 'hello.tar.gz')
+    element_name = 'tar/tar-test.bst'
 
-    result = cli.run(project=project, args=['build', 'tar-test.bst'])
+    result = cli.run(project=project, args=['build', element_name])
     result.assert_success()
 
-    result = cli.run(project=project, args=['artifact', 'checkout', '--directory', checkout_dir, 'tar-test.bst'])
+    result = cli.run(project=project, args=['artifact', 'checkout', '--directory', checkout_dir, element_name])
     result.assert_success()
 
     assert_contains(checkout_dir, ["/hello.tar.gz"])
@@ -34,3 +40,38 @@ def test_tar_build(cli, datafiles):
     tar_hello = tarfile.open(tarpath)
     contents = tar_hello.getnames()
     assert contents == ['hello.c']
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_tar_checksums(cli, datafiles):
+    project = str(datafiles)
+    checkout_dir = os.path.join(cli.directory, 'tar_checkout')
+    element_name = 'tar/tar-test-checksums.bst'
+
+    result = cli.run(project=project, args=['build', element_name])
+    result.assert_success()
+
+    result = cli.run(project=project, args=['artifact', 'checkout', '--directory', checkout_dir, element_name])
+    result.assert_success()
+
+    assert_contains(checkout_dir, ["/hello.tar.gz", "/hello.sha256sum", "/hello.md5sum"])
+
+    with open(os.path.join(checkout_dir, "hello.sha256sum"), 'r') as f:
+        file_sha256 = f.read().strip()
+    with open(os.path.join(checkout_dir, "hello.md5sum"), 'r') as f:
+        file_md5 = f.read().strip()
+    sha256 = hashlib.sha256()
+    md5 = hashlib.md5()
+
+    with open(os.path.join(checkout_dir, "hello.tar.gz"), 'rb') as tar:
+        while True:
+            buf = tar.read(BLOCKSIZE)
+            if not buf:
+                break
+            sha256.update(buf)
+            md5.update(buf)
+
+    actual_sha256 = sha256.hexdigest()
+    actual_md5 = md5.hexdigest()
+    assert file_sha256 == actual_sha256
+    assert file_md5 == actual_md5
