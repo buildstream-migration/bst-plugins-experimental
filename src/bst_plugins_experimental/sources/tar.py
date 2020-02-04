apache/buildstream

tar.py: Make directories with incorrect permissions traversable

src/buildstream/plugins/sources/tar.py
@@ -75,8 +75,12 @@ class ReadableTarInfo(tarfile.TarInfo):
     """
     @property
     def mode(self):
-        # ensure file is readable by owner and group
-        return self.__permission | 0o440
+        # ensure file is readable by owner and group, and executable
+        # (=traversable) if it's a directory.
+        if self.isdir():
+            return self.__permission | 0o550
+        else:
+            return self.__permission | 0o440
 
     @mode.setter
     def mode(self, permission):

buildstream-migration/bst-plugins-experimental

src/bst_plugins_experimental/sources/tar.py
@@ -77,8 +77,12 @@ class ReadableTarInfo(tarfile.TarInfo):
 
     @property
     def mode(self):
-        # ensure file is readable by owner
-        return self.__permission | 0o400
+        # ensure file is readable by owner, and executable
+        # (=traversable) if it's a directory.
+        if self.isdir():
+            return self.__permission | 0o500
+        else:
+            return self.__permission | 0o400
 
     @mode.setter
     def mode(self, permission):