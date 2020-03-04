7a41951bde8d3d41c6904ff17590454b3ffcbc73
Jürg Billeter <j@bitron.ch>
2020-03-04 07:27:40 +0100

    dpkg_build.py: Drop special code path for workspaced elements
    With BuildStream master workspace sources are staged into the sandbox
    just like other sources.

--- src/bst_plugins_experimental/elements/dpkg_build.py
+++ src/bst_plugins_experimental/elements/dpkg_build.py
@@ -163,18 +163,12 @@ class DpkgElement(BuildElement):
         new_package_scripts = Node.from_dict({})
         have_package_scripts = False
         for package in packages:
-            if not self._get_workspace():  # If we're not using a workspace
-                package_path = os.path.join(
-                    sandbox.get_directory(),
-                    self.get_variable("build-root").lstrip(os.sep),
-                    "debian",
-                    package,
-                )
-            else:  # We have a workspace open for this dpkg_build element
-                workspace = self._get_workspace()
-                package_path = os.path.join(
-                    workspace.get_absolute_path(), "debian", package
-                )
+            package_path = os.path.join(
+                sandbox.get_directory(),
+                self.get_variable("build-root").lstrip(os.sep),
+                "debian",
+                package,
+            )
 
             # Exclude DEBIAN files because they're pulled in as public metadata
             contents = [
