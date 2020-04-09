8f7a14c677f5c1e13dfa65b6764346add32bc66b
Darius Makovsky <traveltissues@protonmail.com>
2020-04-09 13:36:46 +0100

    Add BazelizeElement
    This plugin facilitates the limited integration of buildstream
    artifacts into bazel projects. It serves as tooling providing
    bazel BUILD metadata (specifying buildstream artifacts) as part of
    the buildstream build output.
    
    The intended use is as the kind of a dummy element with a
    build-depends field declaring elements required by a bazel project.
    The execution of the buildstream build action will produce a single
    BUILD file specifying the consumption of the relevant artifacts of
    those dependencies. In this way the artifacts will be immediately
    useful to a bazel project without additional development effort.
    
    Elements of this kind permit the following configurations:
    * 'buildfile-dir': the absolute path in the sandbox of the output
    * 'copts': a sequence of strings to pass to the `copts` rule argument
    * 'bazel-rule': the bazel rule to write for this element
      - 'BST.BAZEL_NONE_RULE' is a reserved rule which specifies the
        "empty" rule. Elements with this rule value will not appear in
        the output.
      - the default rule for elements in the tree not of this kind or
        otherwise not specifying the rule will be 'cc_library'
    
    As an example:
    
    ```
    kind: bazelize
    build-depends:
    - foo
    config:
      buildfile-dir: /
      copts: []
      bazel-rule: "BST.BAZEL_NONE_RULE"
    ```
    
    will produce:
    
    ```
    cc_library(
        name = "foo",
        srcs = [],
        hdrs = [],
        deps = []
    )
    ```

---
+++ tests/integration/bazelize.py
@@ -0,0 +1,119 @@
+# Pylint doesn't play well with fixtures and dependency injection from pytest
+# pylint: disable=redefined-outer-name
+
+import os
+import pytest
+
+from buildstream.testing.runcli import (  # pylint: disable=unused-import
+    cli_integration as cli,
+)
+from buildstream.testing.integration import (  # pylint: disable=unused-import
+    integration_cache,
+)
+from buildstream.testing.integration import assert_contains
+from buildstream.testing._utils.site import HAVE_SANDBOX
+
+pytestmark = pytest.mark.integration  # pylint: disable=invalid-name
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "project")
+
+
+def render_entry(entry):
+    """Format a test entry as it is expected to appear in BUILD"""
+    msg = ["{}(".format(entry["rule"]) + os.linesep]  # rule(
+    msg += [
+        '    name = "{}",'.format(entry["name"]) + os.linesep
+    ]  #     name = "name",
+    for item in ["srcs", "hdrs", "deps", "copts"]:
+        if item in entry:
+            msg += [
+                "    {} = {},".format(item, entry[item]) + os.linesep
+            ]  #     item = [values],
+    msg += [")" + os.linesep]  # )
+    return msg
+
+
+@pytest.mark.datafiles(DATA_DIR)
+@pytest.mark.skipif(
+    not HAVE_SANDBOX, reason="Only available with a functioning sandbox"
+)
+# pylint: disable=too-many-locals
+def test_gen_buildrules(cli, datafiles):
+    project = str(datafiles)
+    checkout = os.path.join(project, "checkout")
+    prj_prefix = "bazelize-"
+    element_name = "bazelize/empty.bst"
+    build_file = "BUILD.bazelize-empty"  # default build file name
+
+    # try to build
+    result = cli.run(project=project, args=["build", element_name],)
+    result.assert_success()
+
+    # try to checkout
+    result = cli.run(
+        project=project,
+        args=["artifact", "checkout", "--directory", checkout, element_name],
+    )
+    result.assert_success()
+
+    # check for existence of the build file
+    assert_contains(checkout, [os.path.sep + build_file])
+
+    # format test content to check against the content of the build file
+    # format expected library data
+    lib_prefix = [
+        "usr" + os.path.sep + dirname for dirname in ["testlibs1", "testlibs2"]
+    ]
+    hdr_prefix = [
+        "usr" + os.path.sep + dirname
+        for dirname in ["testincludes1", "testincludes2"]
+    ]
+
+    def get_srcs(lib_num):
+        return [
+            lib_prefix[lib_num - 1] + os.path.sep + lib
+            for lib in ["lib3.so", "lib2.a", "lib1.so"]
+        ]
+
+    def get_hdrs(lib_num):
+        return [
+            hdr_prefix[lib_num - 1] + os.path.sep + hdr
+            for hdr in ["hdr2.h", "hdr1.h", "hdr3.h"]
+        ]
+
+    def gen_cc_lib(num):
+        libname = "makelib" + str(num)
+        return {
+            "rule": "cc_library",
+            "name": prj_prefix + libname,
+            "srcs": sorted([libname] + get_srcs(num)),
+            "hdrs": sorted(get_hdrs(num)),
+        }
+
+    # format expected binary data
+    # FIXME: bin1_srcs are [glob(app/*)] or ["app/afile.cpp", "app/bfile.c"]
+    # see #6
+    bin1_srcs = ["app"]
+    bin1_deps = [prj_prefix + "makelib2", prj_prefix + "makelib1"]
+    bin1_opts = ["-I/lib/inc", "-I/include/someinc"]
+    bin1 = {
+        "rule": "cc_binary",
+        "name": prj_prefix + "bazelize",
+        "srcs": sorted(bin1_srcs),
+        "deps": sorted(bin1_deps),
+        "copts": sorted(bin1_opts),
+    }
+
+    # nb. current rules are sorted by name field in the plugin
+    expected = [
+        'load("@rules_cc//cc:defs.bzl", "cc_binary", "cc_library")'
+        + os.linesep
+    ]
+    expected += render_entry(bin1)
+    expected += render_entry(gen_cc_lib(1))
+    expected += render_entry(gen_cc_lib(2))
+
+    with open(os.path.join(checkout, build_file), "r") as fdata:
+        artifact = fdata.readlines()
+
+    assert artifact == expected
