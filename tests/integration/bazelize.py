1b0a405c3475bb3e0f9dd80374562e368bfa2034
Darius Makovsky <traveltissues@protonmail.com>
2020-05-26 13:29:48 +0100

    bazelize: Add support for cc_import rules
    `cc_import` is intended for including pre-compiled libraries. Any entry
    satisfying the following will be treated as `cc_import`:
    
    * does not have compilation unit sources
    * does not have dependencies, link options, or compilation options
    * has only one source file matching one of the appropriate library
    regexes
    
    There is some overlap in the allowed extensions of shared, static, and
    interface libraries:
    * "*.lib" overlaps in static and interface (assumed to be
    static)
    * "*.so" overlaps in shared and interface (assumed to be shared)
    * "*.dylib" overlaps in interface and shared (assumed to be shared)
    
    It is not possible to decide at a bst level if linking will succeed
    with the values passed so these values are collected based on matching
    the extensions of the files.

--- tests/integration/bazelize.py
+++ tests/integration/bazelize.py
@@ -18,6 +18,42 @@ pytestmark = pytest.mark.integration  # pylint: disable=invalid-name
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "project")
 
+LIB_EXT = [
+    ".so",
+    ".a",
+    ".so.1234.4321",
+    ".pic.a",
+    ".pic.lo",
+    ".lo",
+]
+
+SRC_EXT = [
+    ".c",
+    ".cc",
+    ".cpp",
+    ".cxx",
+    ".c++",
+    ".C",
+    ".S",
+    ".o",
+    ".pic.o",
+]
+
+HDR_EXT = [
+    ".h",
+    ".hh",
+    ".hpp",
+    ".hxx",
+    ".inc",
+    ".inl",
+    ".H",
+]
+
+PREFIX = "usr"
+LIB_PREFIX = os.path.join(PREFIX, "testlibs")
+HDR_PREFIX = os.path.join(PREFIX, "testincs")
+SRC_PREFIX = os.path.join(PREFIX, "testsrcs")
+
 
 def render_entry(entry):
     """Format a test entry as it is expected to appear in BUILD"""
@@ -25,13 +61,40 @@ def render_entry(entry):
     msg += [
         '    name = "{}",'.format(entry["name"]) + os.linesep
     ]  #     name = "name",
-    for item in ["srcs", "hdrs", "deps", "copts", "linkopts"]:
+    for item in [
+        "srcs",
+        "hdrs",
+        "deps",
+        "copts",
+        "linkopts",
+        "interface_library",
+        "shared_library",
+        "static_library",
+    ]:
         if item in entry and entry[item]:
             msg += [
                 "    {} = {},".format(item, entry[item]) + os.linesep
             ]  #     item = [values],
     msg += [")" + os.linesep]  # )
     return msg
+
+
+def get_files(libname, exts, prefix):
+    libs = [libname + lib for lib in exts]
+    random.shuffle(libs)
+    return [os.path.join(prefix, libname, lib) for lib in libs]
+
+
+def get_libs(libname):
+    return get_files(libname, LIB_EXT, LIB_PREFIX)
+
+
+def get_srcs(libname):
+    return get_files(libname, SRC_EXT, SRC_PREFIX)
+
+
+def get_hdrs(libname):
+    return get_files(libname, HDR_EXT, HDR_PREFIX)
 
 
 @pytest.mark.datafiles(DATA_DIR)
@@ -62,60 +125,6 @@ def test_gen_buildrules(cli, datafiles):
 
     # format test content to check against the content of the build file
     # format expected library data
-    prefix = "usr" + os.path.sep
-    lib_prefix = prefix + "testlibs"
-    hdr_prefix = prefix + "testincs"
-    src_prefix = prefix + "testsrcs"
-
-    def get_libs(libname):
-        libs = [
-            libname + lib
-            for lib in [
-                ".so",
-                ".a",
-                ".so.1234.4321",
-                ".pic.a",
-                ".pic.lo",
-                ".lo",
-            ]
-        ]
-        random.shuffle(libs)
-        return [
-            lib_prefix + os.path.sep + libname + os.path.sep + lib
-            for lib in libs
-        ]
-
-    def get_srcs(libname):
-        libs = [
-            libname + lib
-            for lib in [
-                ".c",
-                ".cc",
-                ".cpp",
-                ".cxx",
-                ".c++",
-                ".C",
-                ".S",
-                ".o",
-                ".pic.o",
-            ]
-        ]
-        random.shuffle(libs)
-        return [
-            src_prefix + os.path.sep + libname + os.path.sep + lib
-            for lib in libs
-        ]
-
-    def get_hdrs(libname):
-        hdrs = [
-            libname + hdr
-            for hdr in [".h", ".hh", ".hpp", ".hxx", ".inc", ".inl", ".H",]
-        ]
-        random.shuffle(hdrs)
-        return [
-            hdr_prefix + os.path.sep + libname + os.path.sep + hdr
-            for hdr in hdrs
-        ]
 
     def gen_cc_lib(num):
         libname = "cclib" + str(num)
@@ -154,6 +163,80 @@ def test_gen_buildrules(cli, datafiles):
     expected += render_entry(bin1)
     expected += render_entry(gen_cc_lib(1))
     expected += render_entry(gen_cc_lib(2))
+
+    with open(os.path.join(checkout, build_file), "r") as fdata:
+        artifact = fdata.readlines()
+
+    assert artifact == expected
+
+
+@pytest.mark.datafiles(DATA_DIR)
+@pytest.mark.skipif(
+    not HAVE_SANDBOX, reason="Only available with a functioning sandbox"
+)
+@pytest.mark.xfail(reason="Invalid targets from bst elements", strict=True)
+# pylint: disable=too-many-locals
+def test_gen_ccimports(cli, datafiles):
+    project = str(datafiles)
+    checkout = os.path.join(project, "checkout")
+    prj_prefix = "bazelize-"
+    element_name = "bazelize/imports.bst"
+    build_file = "BUILD"  # default build file name
+
+    # try to build
+    result = cli.run(project=project, args=["build", element_name],)
+    result.assert_success()
+
+    # try to checkout
+    result = cli.run(
+        project=project,
+        args=["artifact", "checkout", "--directory", checkout, element_name],
+    )
+    result.assert_success()
+
+    # check for existence of the build file
+    assert_contains(checkout, [os.path.sep + build_file])
+
+    # format test content to check against the content of the build file
+    # format expected library data
+
+    def gen_cc_lib(lib_type):
+        libname = "ccimp_" + lib_type
+        lib_map = {"interface": ".ifso", "shared": ".so", "static": ".a"}
+        if lib_type == "multi":
+            lib_ext = [lib_map["interface"], lib_map["shared"]]
+        else:
+            lib_ext = [lib_map[lib_type]]
+
+        files = get_files(libname, lib_ext, LIB_PREFIX)
+
+        lib_entries = {}
+
+        for fname in files:
+            for k, v in lib_map.items():
+                if fname.endswith(v):
+                    lib_entries["{}_library".format(k)] = fname
+        entry = {
+            "rule": "cc_import",
+            "name": prj_prefix + libname,
+            "hdrs": sorted(get_hdrs(libname)),
+        }
+
+        for k, v in lib_entries.items():
+            entry[k] = v
+
+        return entry
+
+    # nb. current rules are sorted by name field in the plugin
+    expected = [
+        'package(default_visibility = ["//visibility:public"])' + os.linesep
+    ]
+    expected.extend(
+        ['load("@rules_cc//cc:defs.bzl", "cc_import")' + os.linesep]
+    )
+    lib_types = sorted(["shared", "static", "multi"])
+    for lib_type in lib_types:
+        expected += render_entry(gen_cc_lib(lib_type))
 
     with open(os.path.join(checkout, build_file), "r") as fdata:
         artifact = fdata.readlines()
